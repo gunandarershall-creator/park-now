ea7c2d78efde6f496348a2334a1abad6665d4e4c
gunandarershall-creator <gunandarershall@gmail.com>
2026-04-18 13:55:49 +0100

    fix: move hardcoded firebase config to environment variables in vercel

--- src/models/firebase.js
+++ src/models/firebase.js
@@ -12,12 +12,12 @@ import { getMessaging } from "firebase/messaging";
 const firebaseConfig = typeof window !== 'undefined' && window.__firebase_config
   ? JSON.parse(window.__firebase_config)
   : {
-      apiKey: "AIzaSyA7iaKO86Pbx02BJeA0SulWqpAfB1qs7NU",
-      authDomain: "fyp-backend-parknow.firebaseapp.com",
-      projectId: "fyp-backend-parknow",
-      storageBucket: "fyp-backend-parknow.firebasestorage.app",
-      messagingSenderId: "762323405309",
-      appId: "1:762323405309:web:cca5363efc85606b807194"
+      apiKey:            process.env.REACT_APP_FIREBASE_API_KEY,
+      authDomain:        process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+      projectId:         process.env.REACT_APP_FIREBASE_PROJECT_ID,
+      storageBucket:     process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+      appId:             process.env.REACT_APP_FIREBASE_APP_ID,
     };
 
 export const rawAppId = typeof window !== 'undefined' && window.__app_id
